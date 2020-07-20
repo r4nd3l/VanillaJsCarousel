8b3bf90eb18b05c9fdfcb42546f6f25731e3856b
matemolnar88 <matemolnar88@gmail.com>
2020-07-20 18:25:32 +0200

    first commit

---
+++ js/main.js
@@ -0,0 +1,73 @@
+// Default DevCorner JavaScript Setting
+// Get the modal
+var modal = document.getElementById('_myModal');
+
+// Get the button that opens the modal
+var btn = document.getElementById("_myBtn");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("_close")[0];
+
+// When the user clicks the button, open the modal
+btn.onclick = function() {
+  modal.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
+
+
+const slides = document.querySelectorAll(".slide");
+const nextBtn = document.querySelector(".nextBtn");
+const prevBtn = document.querySelector(".prevBtn");
+slides.forEach(function (slide, index) {
+  slide.style.left = `${index * 100}%`;
+});
+let counter = 0;
+nextBtn.addEventListener("click", function () {
+  counter++;
+  carousel();
+});
+
+prevBtn.addEventListener("click", function () {
+  counter--;
+  carousel();
+});
+
+function carousel() {
+  // working with slides
+  // if (counter === slides.length) {
+  //   counter = 0;
+  // }
+  // if (counter < 0) {
+  //   counter = slides.length - 1;
+  // }
+  // working with buttons
+  if (counter < 0) {
+    counter = 0;
+  }
+  if (counter < slides.length - 1) {
+    nextBtn.style.display = "block";
+  } else {
+    nextBtn.style.display = "none";
+  }
+  if (counter > 0) {
+    prevBtn.style.display = "block";
+  } else {
+    prevBtn.style.display = "none";
+  }
+  slides.forEach(function (slide) {
+    slide.style.transform = `translateX(-${counter * 100}%)`;
+  });
+}
+
+prevBtn.style.display = "none"
---
+++ readme.md
@@ -0,0 +1,6 @@
+Html, Css, VanillaJs - VanillaJsCarousel - Exercise
+> [Preview](https://r4nd3l.github.io/VanillaJsCarousel/)
+---
+
+![VanillaJsCarousel](https://github.com/r4nd3l/VanillaJsCarousel/blob/master/img/sample.png)
+
